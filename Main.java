e819b0cd16857537f6ba63ccda495a96475ffa8a
renan.beraldi <renanberaldi@outlook.com>
2024-11-08 01:01:44 -0300

    Adicionado Laco While e Condicional

--- Main.java
+++ Main.java
@@ -13,5 +13,20 @@ public class Main {
         } else {
             doingCourse = true;
         }
+
+        while (!doingCourse) {
+            System.out.print("E agora ja iniciou? (S/N)");
+            response = scanner.next().charAt(0);
+            if (response == 'N') {
+                doingCourse = false;
+            } else {
+                doingCourse = true;
+                break;
+            }
+        }
+
+        if (doingCourse) {
+            System.out.println("Parabens pela escolha");
+        }
     }
 }
