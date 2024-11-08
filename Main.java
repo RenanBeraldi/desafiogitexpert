fe92cdf076c7572857f6a2bffd6a45f7276ca5d2
renan.beraldi <renanberaldi@outlook.com>
2024-11-08 00:57:16 -0300

    Adicionado interacao com usuario e condicional

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        boolean doingCourse = false;
+        boolean doingCourse;
+
+        System.out.print("Ja iniciou o curso de Git e GitHub Expert? (S/N)");
+        char response = scanner.next().charAt(0);
+        if (response == 'N') {
+            doingCourse =  false;
+        } else {
+            doingCourse = true;
+        }
     }
 }
